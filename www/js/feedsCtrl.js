8c8923777743996eee6e91210e5eeab8a3e406dc
hosDevMac <hosdevmac@hosDevMacs-iMac.local>
2015-11-12 10:22:49 +0100

    fixing a state, moving a requestDetails inside a nested state of home.

--- www/js/feedsCtrl.js
+++ www/js/feedsCtrl.js
@@ -1,4 +1,4 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular.module('BloodDonationApp')
@@ -29,7 +29,7 @@
             return $scope.shownGroup === requests;
         }
         $scope.goToDetails = function() {
-            $state.go('requestDetails', {id : 5});
+            $state.go('home.requestDetails', {id : 5});
         }
        
 
