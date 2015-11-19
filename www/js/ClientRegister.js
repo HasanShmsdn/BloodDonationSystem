179cea924af483f60bf9d009cde4c7ce508366e4
hasanshmsdn <hasanshamseddin@hotmail.com>
2015-11-19 19:26:46 +0200

    adding ClientRegister.js

---
+++ www/js/ClientRegister.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('BloodDonationApp')
+        .controller('ClientRegisterCtrl', ['BloodDonationApi', '$stateParams', '$state', '$scope', ClientRegisterCtrl]);
+
+    function ClientRegisterCtrl(BloodDonationApi, $stateParams, $state, $scope) {
+        var vm = this;
+        var data = BloodDonationApi.getRequests();
+        console.log(data);
+        vm.requestDetails = data;
+        console.log("$stateParams", $stateParams.id);
+
+        //end Data Binding//
+        //  Get you request
+
+       
+        console.log("$stateParams", $stateParams.id);
+
+
+    };
+})();
